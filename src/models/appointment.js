b15445c1632680fb23283ebb429f37d15b50ed20
iDams <damien.soulard@gmail.com>
2013-11-01 00:27:15 +0100

    Small modifications

--- src/models/appointment.js
+++ src/models/appointment.js
@@ -4,15 +4,6 @@ define([
     'backbone'
 ], function($, _, Backbone){
     var Appointment = Backbone.Model.extend({
-        defaults: {
-            title: "New Appointment",
-            startDate: "",
-            startDateFormat: "",
-            startTime: "",
-            endTime: "",
-            name: "",
-            description: ""
-        },
 
         initialize: function(){
             console.log("Creation of a new article");
--- src/views/appointment/appointmentAddEditView.js
+++ src/views/appointment/appointmentAddEditView.js
@@ -7,12 +7,11 @@ define([
 ], function($, _, Backbone, moment, appointmentTemplate){
     var AppointmentAddView = Backbone.View.extend({
         el: $('#appointment-add-container'),
-        formId: '#appt-form',
         editType: 'Edit',
         addType: 'Add',
         currentDate: moment().format('YYYY-MM-DD'),
-        defaultStartTime: '01:00PM',
-        defaultEndTime: '02:00PM',
+        defaultStartTime: '13:00',
+        defaultEndTime: '14:00',
 
         events:{
             'focus .placeholder': 'placeholderTrigger',
@@ -29,41 +28,36 @@ define([
         },
 
         addEditAppt: function(){
-            var title = $(this.formId + ' #title').val();
-            var startDate = $(this.formId + ' #start-date').val();
-            var startTime = $(this.formId + ' #start-time').val();
-            var endTime = $(this.formId + ' #end-time').val();
-            var name = $(this.formId + ' #name').val();
-            var desc = $(this.formId + ' #desc').val();
+            var titleVal = $('#title').val();
+            var title = 'New Appointment';
+            if(titleVal != '')
+                title = titleVal;
+            var startDate = $('#start-date').val();
+            var startTime = $('#start-time').val();
+            var endTime = $('#end-time').val();
+            var name = $('#name').val();
+            var desc = $('#desc').val();
 
-            var startDateFormat = moment(startDate).format('dddd, MMM D, YYYY');
+            var startDateFormat = moment(startDate).format('dddd, MMM D, YYYY').toUpperCase();
+            var model = {title: title,
+                         startDate: startDate,
+                         startDateFormat: startDateFormat,
+                         startTime: startTime,
+                         endTime: endTime,
+                         name: name,
+                         description: desc};
 
             if(this.type == this.addType){
-                this.apptCollection.create({
-                                        title: title,
-                                        startDate: startDate,
-                                        startDateFormat: startDateFormat,
-                                        startTime: startTime,
-                                        endTime: endTime,
-                                        name: name,
-                                        description: desc });
+                this.apptCollection.create(model);
             }
 
             if(this.type == this.editType){
-                this.apptModel.save({
-                                  title: title,
-                                  startDate: startDate,
-                                  startDateFormat: startDateFormat,
-                                  startTime: startTime,
-                                  endTime: endTime,
-                                  name: name,
-                                  description: desc });
+                this.apptModel.save(model);
             }
         },
 
         render: function(){
             $(document).attr('title', 'Meet Me - ' + this.type);
-            this.checkInputTimeFormat();
             var compiledTemplate = _.template(appointmentTemplate, {appt: this.apptModel,
                                                                     currentDate: this.currentDate,
                                                                     defaultStartTime: this.defaultStartTime,
@@ -74,31 +68,9 @@ define([
         },
 
         setMinEndTime: function(){
-            var startTime = $(this.formId + ' #start-time').val();
-            $(this.formId + ' #end-time').val(startTime);
+            var startTime = $('#start-time').val();
+            $('#end-time').val(startTime);
 
-        },
-
-        checkInputTimeFormat: function(){
-            // Not sure about input [type=time] format 12 or 24
-            $(this.formId + '#start-time').val(this.defaultStartTime);
-            if(!$(this.formId + '#start-time').val()){
-                this.defaultStartTime = this.time12Hoursto24Hours(this.defaultStartTime);
-                this.defaultEndTime = this.time12Hoursto24Hours(this.defaultEndTime);
-            }
-        },
-
-        // Format hh:mmPM
-        time12Hoursto24Hours: function(fullTime){
-            var period = fullTime.slice(-2);
-            var time = fullTime.substr(0, fullTime.length - 2);
-            var timeArray = time.split(':');
-
-            if(period == 'PM'){
-                return parseInt(timeArray[0])+12+':'+timeArray[1];
-            } else {
-                return time;
-            }
         },
 
         clear: function(){
